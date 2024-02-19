cf7b374c4696277eb70abd8ad0a034ebeaa346e4
Marc Toensing <marc.toensing@gmail.com>
2024-02-19 23:00:22 +0100

    linted

--- src/mc-players/edit.js
+++ src/mc-players/edit.js
@@ -3,21 +3,13 @@ import ServerSideRender from '@wordpress/server-side-render';
 import { useBlockProps } from '@wordpress/block-editor';
 import './editor.scss';
 
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @return {Element} Element to render.
- */
-export default function Edit(attributes) {
+export default function Edit( attributes ) {
 	return (
 		<p { ...useBlockProps() }>
 			<ServerSideRender
-					block="minecraft-server-info/mc-players"
-					attributes={ attributes }
-				/>
+				block="minecraft-server-info/mc-players"
+				attributes={ attributes }
+			/>
 		</p>
 	);
 }
