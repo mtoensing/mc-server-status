94348371947aec91cbe88b6b935ffa9a9ec17978
Marc Toensing <marc.toensing@gmail.com>
2024-02-19 22:23:59 +0100

    Real build

--- build/mc-players/index.asset.php
+++ build/mc-players/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'c910fea89aa53e3ef6c1');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => '3f73e6b4c75a16b1a647');
--- build/mc-status/index.asset.php
+++ build/mc-status/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => '2304b9e2be46a36425f6');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => '3bd9e3775d00b9fe2e4d');
