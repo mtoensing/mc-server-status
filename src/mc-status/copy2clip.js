f179c5d0e5bfcdbdc5d600e37d557c61c1843e1b
Marc Toensing <marc.toensing@gmail.com>
2024-03-06 19:59:22 +0100

    Update copy2clip.js

--- src/mc-status/copy2clip.js
+++ src/mc-status/copy2clip.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-alert, no-unused-vars, no-undef */
-function copyToClipboard(text) {
+function copyToClipboard( text ) {
 	// Create a temporary input element
-	const tempInput = document.createElement('input');
+	const tempInput = document.createElement( 'input' );
 	tempInput.style = 'position: absolute; left: -1000px; top: -1000px';
 	tempInput.value = text;
-	document.body.appendChild(tempInput);
+	document.body.appendChild( tempInput );
 	tempInput.select();
-	document.execCommand('copy');
-	document.body.removeChild(tempInput);
+	document.execCommand( 'copy' );
+	document.body.removeChild( tempInput );
 
 	// Optionally, display a message to the user indicating the text was copied
-	alert('Copied to clipboard: ' + text);
+	alert( 'Copied to clipboard: ' + text );
 }
