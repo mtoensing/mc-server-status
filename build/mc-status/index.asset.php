a218d59bcd61a55a739712bbdd83de73c972c1b4
Marc Toensing <marc.toensing@gmail.com>
2024-03-11 17:36:22 +0100

    color

--- build/mc-status/index.asset.php
+++ build/mc-status/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '9f7e1737b281bcb1d5ba');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '77f28672e19a795c6236');
