fc4873bc22eb82c000774c0404c590658bf7f16b
Marc Toensing <marc.toensing@gmail.com>
2024-04-27 13:21:44 +0200

    lint 1.3.1

--- inc/iframe_fullscreen.js
+++ inc/iframe_fullscreen.js
@@ -1,32 +1,38 @@
-function handleFullscreen(id) {
-    var userAgent = window.navigator.userAgent;
-    if (supportsFullscreenAPI()) {
-        // Use the Fullscreen API if supported
-        goFullscreen(id);
-    } else {
-        // Fallback or notification if Fullscreen API is not supported
-        window.open(document.getElementById(id).src, '_blank');
-    }
+/* eslint-disable no-alert, no-unused-vars, no-undef */
+function handleFullscreen( id ) {
+	const userAgent = window.navigator.userAgent;
+	if ( supportsFullscreenAPI() ) {
+		// Use the Fullscreen API if supported
+		goFullscreen( id );
+	} else {
+		// Fallback or notification if Fullscreen API is not supported
+		window.open( document.getElementById( id ).src, '_blank' );
+	}
 }
 
-function goFullscreen(id) {
-    var element = document.getElementById(id);
-    // Attempt to request fullscreen mode
-    if (element.requestFullscreen) {
-        element.requestFullscreen();
-    } else if (element.webkitRequestFullscreen) { // Safari
-        element.webkitRequestFullscreen();
-    } else if (element.mozRequestFullScreen) { // Firefox
-        element.mozRequestFullScreen();
-    } else if (element.msRequestFullscreen) { // IE
-        element.msRequestFullscreen();
-    }
+function goFullscreen( id ) {
+	const element = document.getElementById( id );
+	// Attempt to request fullscreen mode
+	if ( element.requestFullscreen ) {
+		element.requestFullscreen();
+	} else if ( element.webkitRequestFullscreen ) {
+		// Safari
+		element.webkitRequestFullscreen();
+	} else if ( element.mozRequestFullScreen ) {
+		// Firefox
+		element.mozRequestFullScreen();
+	} else if ( element.msRequestFullscreen ) {
+		// IE
+		element.msRequestFullscreen();
+	}
 }
 
 // Helper function to check for Fullscreen API support
 function supportsFullscreenAPI() {
-    return document.fullscreenEnabled || 
-           document.webkitFullscreenEnabled || 
-           document.mozFullScreenEnabled || 
-           document.msFullscreenEnabled;
+	return (
+		document.fullscreenEnabled ||
+		document.webkitFullscreenEnabled ||
+		document.mozFullScreenEnabled ||
+		document.msFullscreenEnabled
+	);
 }
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Tags:              Minecraft, Server, Gutenberg
 Contributors:      MarcDK
 Tested up to:      6.5
-Stable tag:        1.3.0
+Stable tag:        1.3.1
 Requires PHP:      8.0
 License:           GPL-2.0-or-later
 License URI:       https://www.gnu.org/licenses/gpl-2.0.html
